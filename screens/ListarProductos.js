7e47adb98f323c63940438b04bbe9b746910fbff
leitolml1 <leonelhd4@gmail.com>
2024-10-15 17:43:58 -0300

    par aosvaldo

--- screens/ListarProductos.js
+++ screens/ListarProductos.js
@@ -5,13 +5,78 @@ import { StyleSheet, Text, View ,Button,Alert,Image,TouchableOpacity} from 'reac
 
 export default function ListarProductos({navigation}) {
   return (
-    <View styles={styles.container}>
+    <View style={styles.container}>
       <Text>HOLA LEITO</Text>
         <Text>Listado Productos</Text>
         <View>
-        <TouchableOpacity onPress={() => navigation.navigate('Carrito')}>
-          <Text>Carrito</Text>
+        <TouchableOpacity onPress={() => navigation.navigate('Carrito')} style={styles.botonNegro}>
+          <Text style={styles.textoBlanco}>Carrito</Text>
         </TouchableOpacity>
+      </View>
+      <View style={styles.divProductos}>
+        <View style={styles.divProducto}>
+          <Text>Coca cola 500ml</Text>
+        <TouchableOpacity onPress={() => navigation.navigate('Carrito')} style={styles.botonAgregar}>
+          <Text>1500$</Text>
+          <Text>Agregar</Text>
+        </TouchableOpacity>
+        </View>
+        <View style={styles.divProducto}>
+          <Text>Coca cola 500ml</Text>
+        <TouchableOpacity onPress={() => navigation.navigate('Carrito')} style={styles.botonAgregar}>
+          <Text>1500$</Text>
+          <Text>Agregar</Text>
+        </TouchableOpacity>
+        </View>
+        <View style={styles.divProducto}>
+          <Text>Coca cola 500ml</Text>
+        <TouchableOpacity onPress={() => navigation.navigate('Carrito')} style={styles.botonAgregar}>
+          <Text>1500$</Text>
+          <Text>Agregar</Text>
+        </TouchableOpacity>
+        </View>
+        <View style={styles.divProducto}>
+          <Text>Coca cola 500ml</Text>
+        <TouchableOpacity onPress={() => navigation.navigate('Carrito')} style={styles.botonAgregar}>
+          <Text>1500$</Text>
+          <Text>Agregar</Text>
+        </TouchableOpacity>
+        </View>
+        <View style={styles.divProducto}>
+          <Text>Coca cola 500ml</Text>
+        <TouchableOpacity onPress={() => navigation.navigate('Carrito')} style={styles.botonAgregar}>
+          <Text>1500$</Text>
+          <Text>Agregar</Text>
+        </TouchableOpacity>
+        </View>
+        <View style={styles.divProducto}>
+          <Text>Coca cola 500ml</Text>
+        <TouchableOpacity onPress={() => navigation.navigate('Carrito')} style={styles.botonAgregar}>
+          <Text>1500$</Text>
+          <Text>Agregar</Text>
+        </TouchableOpacity>
+        </View>
+        <View style={styles.divProducto}>
+          <Text>Coca cola 500ml</Text>
+        <TouchableOpacity onPress={() => navigation.navigate('Carrito')} style={styles.botonAgregar}>
+          <Text>1500$</Text>
+          <Text>Agregar</Text>
+        </TouchableOpacity>
+        </View>
+        <View style={styles.divProducto}>
+          <Text>Coca cola 500ml</Text>
+        <TouchableOpacity onPress={() => navigation.navigate('Carrito')} style={styles.botonAgregar}>
+          <Text>1500$</Text>
+          <Text>Agregar</Text>
+        </TouchableOpacity>
+        </View>
+        <View style={styles.divProducto}>
+          <Text>Coca cola 500ml</Text>
+        <TouchableOpacity onPress={() => navigation.navigate('Carrito')} style={styles.botonAgregar}>
+          <Text>1500$</Text>
+          <Text>Agregar</Text>
+        </TouchableOpacity>
+        </View>
       </View>
     </View>
   );
@@ -21,9 +86,30 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#c6c6c6',
     alignItems: 'center',
-    justifyContent: 'center',
     padding:20,
     margin:10,
+  },
+  divProducto:{
+    marginTop:"10px",
+    justifyContent: 'space-between',
+    height:"50px",
+    borderRadius:"5px",
+    flexDirection:"row",
+    backgroundColor:"#c6c6c6",
+  },
+  botonNegro:{
+    color:"white",
+    borderRadius:"10px",
+    backgroundColor:"black",
+    padding:"10px",
+  },
+  textoBlanco:{
+    color:"white",
+  },
+  botonAgregar:{
+    borderRadius:"10px",
+    backgroundColor:"green",
+    padding:"10px",
   },
   top:{
     marginTop:"10px",
@@ -44,5 +130,11 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     marginRight:"10px",
+  },
+  divProductos:{
+    padding:"10px",
+    backgroundColor:"white",
+    width:"350px",
+    height:"600px",
   }
 });
