97935d66aa4e27e8b5b4b22c5eacd1ee4101b1fd
leitolml1 <leonelhd4@gmail.com>
2024-10-16 15:21:17 -0300

    xd

--- screens/ListarProductos.js
+++ screens/ListarProductos.js
@@ -1,140 +1,167 @@
 import React from 'react'
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View ,Button,Alert,Image,TouchableOpacity} from 'react-native';
+import { StyleSheet, Text, View ,Button,Alert,TouchableOpacity,Image} from 'react-native';
 
 
 export default function ListarProductos({navigation}) {
   return (
     <View style={styles.container}>
-      <Text>HOLA LEITO</Text>
-        <Text>Listado Productos</Text>
+      <Text>Bienvenido Huesped </Text>
+      <View style={styles.divBotones}>
         <View>
-        <TouchableOpacity onPress={() => navigation.navigate('Carrito')} style={styles.botonNegro}>
-          <Text style={styles.textoBlanco}>Carrito</Text>
-        </TouchableOpacity>
+          <TouchableOpacity onPress={() => navigation.navigate('Carrito')} style={styles.boton}>
+              <Text style={styles.textoBoton}>PRODUCTOS</Text>
+          </TouchableOpacity>
+        </View>
+        <View>
+          <TouchableOpacity onPress={() => navigation.navigate('Carrito')} style={styles.boton}>
+              <Text style={styles.textoBoton}>CARRITO</Text>
+          </TouchableOpacity>
+        </View>
       </View>
       <View style={styles.divProductos}>
-        <View style={styles.divProducto}>
-          <Text>Coca cola 500ml</Text>
-        <TouchableOpacity onPress={() => navigation.navigate('Carrito')} style={styles.botonAgregar}>
-          <Text>1500$</Text>
-          <Text>Agregar</Text>
-        </TouchableOpacity>
-        </View>
-        <View style={styles.divProducto}>
-          <Text>Coca cola 500ml</Text>
-        <TouchableOpacity onPress={() => navigation.navigate('Carrito')} style={styles.botonAgregar}>
-          <Text>1500$</Text>
-          <Text>Agregar</Text>
-        </TouchableOpacity>
-        </View>
-        <View style={styles.divProducto}>
-          <Text>Coca cola 500ml</Text>
-        <TouchableOpacity onPress={() => navigation.navigate('Carrito')} style={styles.botonAgregar}>
-          <Text>1500$</Text>
-          <Text>Agregar</Text>
-        </TouchableOpacity>
-        </View>
-        <View style={styles.divProducto}>
-          <Text>Coca cola 500ml</Text>
-        <TouchableOpacity onPress={() => navigation.navigate('Carrito')} style={styles.botonAgregar}>
-          <Text>1500$</Text>
-          <Text>Agregar</Text>
-        </TouchableOpacity>
-        </View>
-        <View style={styles.divProducto}>
-          <Text>Coca cola 500ml</Text>
-        <TouchableOpacity onPress={() => navigation.navigate('Carrito')} style={styles.botonAgregar}>
-          <Text>1500$</Text>
-          <Text>Agregar</Text>
-        </TouchableOpacity>
-        </View>
-        <View style={styles.divProducto}>
-          <Text>Coca cola 500ml</Text>
-        <TouchableOpacity onPress={() => navigation.navigate('Carrito')} style={styles.botonAgregar}>
-          <Text>1500$</Text>
-          <Text>Agregar</Text>
-        </TouchableOpacity>
-        </View>
-        <View style={styles.divProducto}>
-          <Text>Coca cola 500ml</Text>
-        <TouchableOpacity onPress={() => navigation.navigate('Carrito')} style={styles.botonAgregar}>
-          <Text>1500$</Text>
-          <Text>Agregar</Text>
-        </TouchableOpacity>
-        </View>
-        <View style={styles.divProducto}>
-          <Text>Coca cola 500ml</Text>
-        <TouchableOpacity onPress={() => navigation.navigate('Carrito')} style={styles.botonAgregar}>
-          <Text>1500$</Text>
-          <Text>Agregar</Text>
-        </TouchableOpacity>
-        </View>
-        <View style={styles.divProducto}>
-          <Text>Coca cola 500ml</Text>
-        <TouchableOpacity onPress={() => navigation.navigate('Carrito')} style={styles.botonAgregar}>
-          <Text>1500$</Text>
-          <Text>Agregar</Text>
-        </TouchableOpacity>
-        </View>
+              <View style={styles.divProducto}>
+                <Text>
+                  Coca cola 500 ml
+                </Text>
+                <TouchableOpacity style={styles.botonAñadir}>
+                    <Text style={styles.textoPrecio}>1500$</Text>
+                    <Text style={styles.textoAñadir}>AÑADIR</Text>
+                </TouchableOpacity>
+              </View>
+              <View style={styles.divProducto}>
+                <Text>
+                  Coca cola 500 ml
+                </Text>
+                <TouchableOpacity style={styles.botonAñadir}>
+                    <Text style={styles.textoPrecio}>1500$</Text>
+                    <Text style={styles.textoAñadir}>AÑADIR</Text>
+                </TouchableOpacity>
+              </View>
+              <View style={styles.divProducto}>
+                <Text>
+                  Coca cola 500 ml
+                </Text>
+                <TouchableOpacity style={styles.botonAñadir}>
+                    <Text style={styles.textoPrecio}>1500$</Text>
+                    <Text style={styles.textoAñadir}>AÑADIR</Text>
+                </TouchableOpacity>
+              </View>
+              <View style={styles.divProducto}>
+                <Text>
+                  Coca cola 500 ml
+                </Text>
+                <TouchableOpacity style={styles.botonAñadir}>
+                    <Text style={styles.textoPrecio}>1500$</Text>
+                    <Text style={styles.textoAñadir}>AÑADIR</Text>
+                </TouchableOpacity>
+              </View>
+              <View style={styles.divProducto}>
+                <Text>
+                  Coca cola 500 ml
+                </Text>
+                <TouchableOpacity style={styles.botonAñadir}>
+                    <Text style={styles.textoPrecio}>1500$</Text>
+                    <Text style={styles.textoAñadir}>AÑADIR</Text>
+                </TouchableOpacity>
+              </View>
+              <View style={styles.divProducto}>
+                <Text>
+                  Coca cola 500 ml
+                </Text>
+                <TouchableOpacity style={styles.botonAñadir}>
+                    <Text style={styles.textoPrecio}>1500$</Text>
+                    <Text style={styles.textoAñadir}>AÑADIR</Text>
+                </TouchableOpacity>
+              </View>
+              <View style={styles.divProducto}>
+                <Text>
+                  Coca cola 500 ml
+                </Text>
+                <TouchableOpacity style={styles.botonAñadir}>
+                    <Text style={styles.textoPrecio}>1500$</Text>
+                    <Text style={styles.textoAñadir}>AÑADIR</Text>
+                </TouchableOpacity>
+              </View>
+              <View style={styles.divProducto}>
+                <Text>
+                  Coca cola 500 ml
+                </Text>
+                <TouchableOpacity style={styles.botonAñadir}>
+                    <Text style={styles.textoPrecio}>1500$</Text>
+                    <Text style={styles.textoAñadir}>AÑADIR</Text>
+                </TouchableOpacity>
+              </View>
+              <View style={styles.divProducto}>
+                <Text>
+                  Coca cola 500 ml
+                </Text>
+                <TouchableOpacity style={styles.botonAñadir}>
+                    <Text style={styles.textoPrecio}>1500$</Text>
+                    <Text style={styles.textoAñadir}>AÑADIR</Text>
+                </TouchableOpacity>
+              </View>
+
       </View>
+
+
+
     </View>
   );
 }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#c6c6c6',
+    backgroundColor: '#white',
     alignItems: 'center',
     padding:20,
     margin:10,
   },
-  divProducto:{
-    marginTop:"10px",
-    justifyContent: 'space-between',
-    height:"50px",
-    borderRadius:"5px",
-    flexDirection:"row",
-    backgroundColor:"#c6c6c6",
+  divBotones:{
+    flexDirection: 'row'  ,
   },
-  botonNegro:{
+  textoBoton:{
     color:"white",
-    borderRadius:"10px",
+    textAlign:"center",
+    fontSize:"15px"
+  },
+  boton:{
     backgroundColor:"black",
-    padding:"10px",
-  },
-  textoBlanco:{
-    color:"white",
-  },
-  botonAgregar:{
-    borderRadius:"10px",
-    backgroundColor:"green",
-    padding:"10px",
-  },
-  top:{
-    marginTop:"10px",
-    flex:0.1,
-    backgroundColor:"blue",
-    borderWidth:5,
-  },
-  titulo:{
-    fontSize:20,
-  },
-  medio:{
-    marginTop:"10px",
-    flex:0.5,
-    backgroundColor:"green",
-    borderWidth:5,
-  },
-  botones:{
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginRight:"10px",
+    borderWidth:3,
+    borderRadius:"5px",
+    marginHorizontal: 15, 
   },
   divProductos:{
-    padding:"10px",
-    backgroundColor:"white",
-    width:"350px",
-    height:"600px",
-  }
+    marginTop:"10px",
+    borderRadius:"10px",
+    width:"300px",
+    height:"450px",
+
+  },
+  divProducto:{
+    marginTop:"5px",
+    borderRadius:"10px",
+    backgroundColor:"#d3d3d3",
+    flexDirection: 'row', // Alinear horizontalmente
+    justifyContent: 'space-between', // Poner el botón a la derecha
+    alignItems: 'center', // Centrar verticalmente
+    marginBottom: 10,
+    padding:"20px",
+    height:"10px",
+
+  },
+  botonAñadir:{
+    borderRadius:"10px",
+    backgroundColor:"#08D85A",
+    width:"80px",
+    fontSize:"10px",
+  },
+  textoAñadir:{
+    textAlign:"center",
+    color:"white",
+  },
+  textoPrecio:{
+    textAlign:"center",
+    color:"white",
+  },
 });
